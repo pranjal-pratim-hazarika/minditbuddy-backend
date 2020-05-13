3361deb56ba508fe8f0c5f33fbed2ebede0c1a27
Pranjal <pranjalpratimh@gmail.com>
2020-05-13 20:56:21 +0530

    1st commit by pranjal

---
+++ src/middleware/middleware.js
@@ -0,0 +1,41 @@
+require("dotenv").config({ path: "../config/.env" });
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_KEY;
+
+const checkToken = (req, res, next) => {
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+
+  if (token) {
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length);
+    }
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.set({
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        });
+        return res.json({
+          success: false,
+          message: "Token is not valid"
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.set({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    });
+    return res.json({
+      success: false,
+      message: "Auth token is not supplied"
+    });
+  }
+};
+
+module.exports = {
+  checkToken: checkToken
+};
